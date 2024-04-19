ee19d6bd6ee195e68cdbd3583ecb8b1d484ce83d
HesamPourabbasian <137700266+HesamPourabbasian@users.noreply.github.com>
2024-04-19 15:32:16 +0330

    Update JS.js

--- Script/JS.js
+++ Script/JS.js
@@ -1,4 +1,4 @@
 function change()
 {
-    document.getElementById("bd").style.backgroundImage="url(./img/Black.png)";
+    document.getElementById("bd").style.backgroundImage="url(img/papers.co-mk52-city-night-dark-skyline-architecture-river-25-wallpaper.jpg)";
 }
